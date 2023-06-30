3b4ad9f7f3b5504ea92ae32b0e2acc05f0f9b495
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-29 21:07:51 -0500

    Added button on hover effects and text effect changes

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,6 @@
+"use strict";
+
+var _youtubeExec = require("youtube-exec");
 // // const { dlAudio } = require("youtube-exec");
 // import { dlAudio } from "youtube-exec";
 
@@ -22,26 +25,23 @@
 //     });
 // });
 
-
-import { dlAudio } from "youtube-exec";
-
-const field = document.getElementById('myForm').addEventListener('submit', function(event) {
-  console.log(1)
+var field = document.getElementById('myForm').addEventListener('submit', function (event) {
+  console.log(1);
   event.preventDefault(); // Prevent form submission
   var inputText = document.getElementById('myInput').value;
   console.log('Input:', inputText);
   // Use the inputText variable as needed
 
-  dlAudio({
+  (0, _youtubeExec.dlAudio)({
     url: inputText,
-    folder: "downloads", // optional, default: "youtube-exec"
-    filename: "filename", // optional, default: video title
-    quality: "best", // or "lowest"; default: "best"
-  })
-    .then(() => {
-      console.log("Audio downloaded successfully! 🔊🎉");
-    })
-    .catch((err) => {
-      console.error("An error occurred:", err.message);
-    });
+    folder: "downloads",
+    // optional, default: "youtube-exec"
+    filename: "filename",
+    // optional, default: video title
+    quality: "best" // or "lowest"; default: "best"
+  }).then(function () {
+    console.log("Audio downloaded successfully! 🔊🎉");
+  })["catch"](function (err) {
+    console.error("An error occurred:", err.message);
+  });
 });
