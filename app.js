6a00b9ee7030da925bd584aae794bf01bb18b7f6
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-02 17:27:13 -0500

    okay it looks decent on small sreen sizes now, galaxy fold view was looking odd, but it looks ok now

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mainRoute = require('./routes/frontPage');
-const convertRoute = require('./routes/convert');
+// const convertRoute = require('./src/convert');
 
 // Get port from environment variables or use 3000 as a default
 const port = process.env.PORT || 3000;
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json()); // Middleware to parse JSON requests
 app.use('/', mainRoute);
-app.use('/convert', convertRoute); // Add the convertRoute
+// app.use('.src/convert', convertRoute); // Add the convertRoute
 
 // Central error handler
 app.use((err, req, res, next) => {
