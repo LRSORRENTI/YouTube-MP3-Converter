0214d1f577df9e880f3c0dec86fc0d724212abf8
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-02 19:32:48 -0500

    okay it's now grabbing the text from the popup, then using that as the file name

--- routes/convert.js
+++ routes/convert.js
@@ -1,3 +1,32 @@
+// const express = require('express');
+// const router = express.Router();
+// const ytdl = require('ytdl-core');
+// const ffmpeg = require('fluent-ffmpeg');
+// const fs = require('fs');
+
+// router.post('/', async (req, res) => {
+//     const url = req.body.url;
+//     const filename = Date.now() + '.mp3'; // create a unique filename for each conversion
+
+//     if (ytdl.validateURL(url)) {
+//         let stream = ytdl(url, { filter: 'audioonly' });
+        
+//         await new Promise((resolve, reject) => {
+//             ffmpeg(stream)
+//                 .audioBitrate(320)
+//                 .toFormat('mp3')
+//                 .saveToFile(`./public/${filename}.mp3`)
+//                 .on('end', resolve)
+//                 .on('error', reject);
+//         })
+//         // send the link to the mp3 file
+//         res.json({ link: `/${filename}.mp3` });
+//     } else {
+//         res.status(400).json({ status: 'error', message: 'Invalid YouTube URL' });
+//     }
+// });
+
+// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const ytdl = require('ytdl-core');
@@ -6,7 +35,7 @@ const fs = require('fs');
 
 router.post('/', async (req, res) => {
     const url = req.body.url;
-    const filename = Date.now() + '.mp3'; // create a unique filename for each conversion
+    const filename = req.body.filename || Date.now(); // use the provided filename or a timestamp if no filename was provided
 
     if (ytdl.validateURL(url)) {
         let stream = ytdl(url, { filter: 'audioonly' });
@@ -15,13 +44,13 @@ router.post('/', async (req, res) => {
             ffmpeg(stream)
                 .audioBitrate(320)
                 .toFormat('mp3')
-                .saveToFile(`./public/${filename}`)
+                .saveToFile(`./public/${filename}.mp3`) // include .mp3 extension
                 .on('end', resolve)
                 .on('error', reject);
         });
 
         // send the link to the mp3 file
-        res.json({ link: `/${filename}` });
+        res.json({ link: `/${filename}.mp3` }); // include .mp3 extension
     } else {
         res.status(400).json({ status: 'error', message: 'Invalid YouTube URL' });
     }
